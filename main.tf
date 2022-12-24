208a63caae9bdce8e1a265fecb64b4f88d59103d
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-24 11:52:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-resource "aws_vpc" "my_vpc1" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "mml-example"
-  }
-}
-
-resource "aws_subnet" "my_subnet1" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "mml-example"
-  }
-}
-
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
-
 data "aws_ami" "app_ami" {
   most_recent = true
 
