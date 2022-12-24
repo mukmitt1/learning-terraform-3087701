a7430718a9ffc8e5e5a58acbb8f82193149a1e76
mukmitt1 <76959601+mukmitt1@users.noreply.github.com>
2022-12-24 11:35:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ resource "aws_vpc" "my_vpc1" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "tf-example"
+    Name = "mml-example"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "my_subnet1" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "tf-example"
+    Name = "mml-example"
   }
 }
 
